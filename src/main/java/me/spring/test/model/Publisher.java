60f8da3847c4489ccbb0ac8f521e7d3c8b52290e
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-03 03:36:04 +0300

    Added to Publishers an ability to introduce themself by first and second name or by nick

--- src/main/java/me/spring/test/model/Publisher.java
+++ src/main/java/me/spring/test/model/Publisher.java
@@ -22,4 +22,23 @@ public class Publisher {
     private String nick;
     @Getter @Setter
     private String address;
+
+    public String introduce(){
+        if(firstName != null || lastName != null) return introduceByRealName();
+        return introduceByNick();
+    }
+
+    private String introduceByNick() {
+        return nick;
+    }
+
+    private String introduceByRealName() {
+        String name = "";
+        if(firstName != null) name = firstName;
+        if(lastName != null){
+            if(!name.isEmpty()) name = name.concat(" ");
+            name = name.concat(lastName);
+        }
+        return name;
+    }
 }
