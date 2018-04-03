dd4e9c1e54f1f77b5e9d383c4f0fa80d80b517fb
Anton Maximenkov <maximenkovanton@gmail.com>
2018-04-03 03:54:50 +0300

    copy-paste fix

--- src/main/java/me/spring/test/controller/PublisherController.java
+++ src/main/java/me/spring/test/controller/PublisherController.java
@@ -1,7 +1,7 @@
 package me.spring.test.controller;
 
 import lombok.AllArgsConstructor;
-import me.spring.test.repositories.AuthorRepository;
+import me.spring.test.repositories.PublisherRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @AllArgsConstructor
 public class PublisherController {
-    private AuthorRepository authorRepository;
+    private PublisherRepository publisherRepository;
 
     @RequestMapping("/publishers")
     public String getBook(Model model){
-        model.addAttribute("publishers", authorRepository.findAll());
+        model.addAttribute("publishers", publisherRepository.findAll());
         return "publishers";
     }
 }
